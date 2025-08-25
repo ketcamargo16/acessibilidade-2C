c3d57b39a0b34de163d2f2e81ca48baf13395219
ketcamargo16 <0000110714324xsp@aluno.educacao.sp.gov.br>
2025-08-25 08:34:31 -0300

    v3

--- script.js
+++ script.js
@@ -13,8 +13,15 @@ document.addEventListener('DOMContentLoald', function(){
  diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -=0.1;
         document.body.style.frontSize =`${tamanhoAtualFonte}rem`
-         
-     })
+        })
+const botaoAcessibilidade= document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
+
+botaoAcessibilidade.addEventListener('click', function(){
+   
+})
+
+
  
 
 })
