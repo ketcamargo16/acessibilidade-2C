e8f8d6ae62668e3c15f06a926afc7b22c04bc5ef
ketcamargo16 <0000110714324xsp@aluno.educacao.sp.gov.br>
2025-09-01 07:16:32 -0300

    v1

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
 
 botaoAcessibilidade.addEventListener('click', function(){
    botaoAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
 })
 
 
