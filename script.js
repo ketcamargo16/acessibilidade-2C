0b0c8752f9f22d35c3c0324084d48f5e057854d1
ketcamargo16 <0000110714324xsp@aluno.educacao.sp.gov.br>
2025-09-01 07:23:05 -0300

    a

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-document.addEventListener('DOMContentLoald', function(){
-    const aumentaFonteBotao= document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao= document.getElementById('diminuir-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-      let tamanhoAtualFonte= 1;
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-     aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte +=0.1;
-        document.body.style.frontSize =`${tamanhoAtualFonte}rem`
-         
-     })
+    })
 
- diminuirFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -=0.1;
-        document.body.style.frontSize =`${tamanhoAtualFonte}rem`
-        })
-const botaoAcessibilidade= document.getElementById('botao-acessibilidade');
-const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-botaoAcessibilidade.addEventListener('click', function(){
-   botaoAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    let tamanhoAtualFonte = 1;
 
-})
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    })
 
- 
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    })
 })
