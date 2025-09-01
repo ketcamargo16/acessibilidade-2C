9f7d9e38afcc08f331a14f42e0f8388820b86e20
ketcamargo16 <0000110714324xsp@aluno.educacao.sp.gov.br>
2025-09-01 08:17:54 -0300

    v4

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+     const alternaContraste = document.getElementById('alterna-contraste');
 
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
 
     })
+
+    alternaContraste.addEventListener('click', function(){
+      document.body.classList.toggle('alto-contraste')
+    })
+
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
